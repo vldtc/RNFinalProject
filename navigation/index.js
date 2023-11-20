1e71e7c732e84657c0245c3a94c324724805f028
vldtc <vladut.cirlan@informationtechconsultants.co.uk>
2023-11-20 15:08:46 +0000

    rn navigation stacks

--- navigation/index.js
+++ navigation/index.js
@@ -1,15 +1,37 @@
-import {View, Text} from 'react-native';
-import React from 'react';
+import React, {useState} from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {AuthScreen, HomeScreen} from '../screens';
 
 const Stack = createNativeStackNavigator();
 
 const Navigator = () => {
+  const [isUserLoggedIn, setIsUserLoggedIn] = useState(false);
+
+  const authStack = () => {
+    return (
+      <Stack.Group
+        screenOptions={{
+          headerShown: false,
+        }}>
+        <Stack.Screen name="Auth" component={AuthScreen} />
+      </Stack.Group>
+    );
+  };
+
+  const mainStack = () => {
+    return (
+      <Stack.Group
+        screenOptions={{
+          headerShown: false,
+        }}>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Group>
+    );
+  };
+
   return (
     <Stack.Navigator>
-      <Stack.Screen name="Auth" component={AuthScreen} />
-      <Stack.Screen name="Home" component={HomeScreen} />
+      {isUserLoggedIn ? mainStack() : authStack()}
     </Stack.Navigator>
   );
 };
