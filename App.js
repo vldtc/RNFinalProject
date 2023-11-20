a37c184b63d32cf7ce1b50d69f2ac1a2bcb516a6
vldtc <vladut.cirlan@informationtechconsultants.co.uk>
2023-11-20 14:40:16 +0000

    first commit

---
+++ App.js
@@ -0,0 +1,13 @@
+import {SafeAreaView} from 'react-native';
+import React from 'react';
+import {AuthScreen} from './screens';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <AuthScreen />
+    </SafeAreaView>
+  );
+};
+
+export default App;
---
+++ screens/index.js
@@ -0,0 +1,4 @@
+import AuthScreen from './authScreen';
+import HomeScreen from './homeScreen';
+
+export {AuthScreen, HomeScreen};
