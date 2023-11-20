de02885f85ebed086038974a1b0027ad882065bf
vldtc <vladut.cirlan@informationtechconsultants.co.uk>
2023-11-20 15:20:19 +0000

    rnStackFinished

---
+++ screens/dashScreen/index.js
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+const DashScreen = () => {
+  return (
+    <View>
+      <Text>DashScreen</Text>
+    </View>
+  );
+};
+
+export default DashScreen;
